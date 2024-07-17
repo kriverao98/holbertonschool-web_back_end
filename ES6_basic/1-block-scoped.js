82384f9f0b9e818474b7adfbdcabdad23677e368
kriverao98 <riveraocasiokoral29@gmail.com>
2024-07-17 14:36:29 -0400

    fixed task 1

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -3,8 +3,8 @@ export default function taskBlock(trueOrFalse) {
     const task2 = true;
   
     if (trueOrFalse) {
-      task = true;
-      task2 = false;
+      let new_task = true;
+      let new_task2 = false;
     }
   
     return [task, task2];
