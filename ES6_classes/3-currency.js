3fa06729e5aa8e8b7272dc03cbcd70f087f71879
kriverao98 <riveraocasiokoral29@gmail.com>
2024-07-19 13:11:26 -0400

    feat: Add Currency class with code and name properties, and displayFullCurrency method

---
+++ ES6_classes/3-currency.js
@@ -0,0 +1,42 @@
+export default class Currency {
+  constructor(code, name) {
+    if (typeof code !== 'string') {
+      throw new TypeError('code should be a string');
+    }
+    if (typeof name !== 'string') {
+      throw new TypeError('name should be a string');
+    }
+
+    this._code = code;
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+  // Getter and setter for code
+
+  set code(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('code should be a string');
+    }
+    this._code = value;
+  }
+
+  // Getter and setter for name
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('name should be a string');
+    }
+    this._name = value;
+  }
+
+  // Method to display full currency
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
