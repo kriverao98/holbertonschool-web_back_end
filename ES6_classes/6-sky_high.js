b25823c37c3ae12ebae5cf63943b00af85f5ba7a
kriverao98 <riveraocasiokoral29@gmail.com>
2024-07-19 13:50:26 -0400

    feat: Add SkyHighBuilding class with sqft and floors properties, and override evacuationWarningMessage method

---
+++ ES6_classes/6-sky_high.js
@@ -0,0 +1,30 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+  // Ensure sqft is passed to the parent class
+    super(sqft);
+
+    // Type checks
+    if (typeof floors !== 'number') {
+      throw new TypeError('floors should be a number');
+    }
+
+    this._floors = floors;
+  }
+
+  // Getter for sqft
+  get sqft() {
+    return this._sqft;
+  }
+
+  // Getter for floors
+  get floors() {
+    return this._floors;
+  }
+
+  // Override the evacuationWarningMessage method
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors.`;
+  }
+}
