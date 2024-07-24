4ed12c7514bb31d6f53c7ff9f8e900a744fe8df5
kriverao98 <riveraocasiokoral29@gmail.com>
2024-07-23 22:23:21 -0400

    refactor: Update cleanSet function to handle empty startString parameter

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,8 @@
-function cleanSet(set, startString) {
+function cleanSet(set, startString = '') {
+  if (typeof startString !== 'string') {
+    return '';
+  }
+
   const result = Array.from(set)
     .filter((value) => value.startsWith(startString))
     .map((value) => value.slice(startString.length))
