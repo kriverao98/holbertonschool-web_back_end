fa467b209c7b4092355b4fd691ae4c860db58226
kriverao98 <riveraocasiokoral29@gmail.com>
2024-07-23 21:42:13 -0400

    feat: Add getStudentIdsSum function to calculate the sum of student IDs

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,9 +1,3 @@
-/**
- * Function to get students located in a specific city
- * @param {Array} students - Array of student objects
- * @param {string} city - City to filter students by
- * @returns {Array} - Array of students located in the specified city
- */
 function getStudentsByLocation(students, city) {
   if (!Array.isArray(students) || typeof city !== 'string') {
     return [];
