c23ce14a479e24e7c34d56a924565677e1c30475
kriverao98 <riveraocasiokoral29@gmail.com>
2024-07-25 19:24:54 -0400

    refactor: Update handleResponseFromAPI to use arrow function syntax

---
+++ ES6_promise/2-then.js
@@ -0,0 +1,18 @@
+export default function handleResponseFromAPI(promise) {
+  return new Promise((resolve, reject) => {
+    promise
+      .then(() => {
+        const response = {
+          status: 200,
+          body: 'Success',
+        };
+        resolve(response);
+      })
+      .catch(() => {
+        reject(new Error(''));
+      });
+  }).then((response) => {
+    console.log('Got a response from the API');
+    return response;
+  });
+}
