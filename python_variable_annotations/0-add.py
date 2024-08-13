d635cf76e89027e6b3971f84210c4d9ad4b29821
kriverao98 <riveraocasiokoral29@gmail.com>
2024-08-13 17:23:19 -0400

    refactor: Add type annotations to the add function

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
+"""Function test py type-annotation returning variable a + b"""
 
-def add (a: float, b: float) -> float:
+
+def add(a: float, b: float) -> float:
     """Function takes variables a & b and return the sum of them"""
     return a + b
