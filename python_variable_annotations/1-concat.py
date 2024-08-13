26b9a25dbb822c3b2305acbbc60fa0fd1fdaf02d
kriverao98 <riveraocasiokoral29@gmail.com>
2024-08-13 17:28:10 -0400

    refactor: Add type annotations to the concat function

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Function test type-annotation on a function
+that returns a concatenated string"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Function returns a concatebated string"""
+    return str1 + str2
