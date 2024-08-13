d241b80cb74b2d470b1b5fb0cc157f6fe9ae2693
kriverao98 <riveraocasiokoral29@gmail.com>
2024-08-13 17:38:51 -0400

    refactor: Add type annotations to the floor function

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Function takes a n argument and returns the floor of the float"""
+from math import floor as math_floor
+
+
+def floor(n: float) -> int:
+    """Function takes a n argument and returns the floor of the float"""
+    return math_floor(n)
