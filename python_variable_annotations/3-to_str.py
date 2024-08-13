c02b36ce17191970d3ce318c4309b7b1d237b621
kriverao98 <riveraocasiokoral29@gmail.com>
2024-08-13 17:43:27 -0400

    refactor: Add type annotations to the to_str function

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Function to test type-annotation returning a string
+representation of the float"""
+
+
+def to_str(n: float) -> str:
+    """Function returns a string representation of float n"""
+    return str(n)
