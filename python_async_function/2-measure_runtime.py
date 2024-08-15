ec8af84ed18f25caddfec35c9c5027efba1e50c4
kriverao98 <riveraocasiokoral29@gmail.com>
2024-08-15 18:54:29 -0400

    refactor: Remove unused code and update Python Async project to use concurrent coroutines

---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,10 @@
+import time
+import asyncio
+from concurrent_coroutines import wait_n
+
+def measure_time(n: int, max_delay: int) -> float:
+    start_time = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    end_time = time.time()
+    total_time = end_time - start_time
+    return total_time / n
