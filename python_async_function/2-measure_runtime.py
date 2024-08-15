c02a6b5288b86957a68d4f1aeda82497b16f2ddc
kriverao98 <riveraocasiokoral29@gmail.com>
2024-08-15 19:03:44 -0400

    refactor: Measure average runtime of wait_n function in Python Async project

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,8 +1,19 @@
+#!/usr/bin/env python3
+"""Task test async functions"""
 import time
 import asyncio
 from concurrent_coroutines import wait_n
 
 def measure_time(n: int, max_delay: int) -> float:
+    """
+    Measure the average runtime of the wait_n function by running it 'n' times with a maximum delay of 'max_delay'.
+    Args:
+        n (int): The number of times to run the wait_n function.
+        max_delay (int): The maximum delay for each call to the wait_n function.
+    Returns:
+        float: The average runtime of the wait_n function.
+    """
+
     start_time = time.time()
     asyncio.run(wait_n(n, max_delay))
     end_time = time.time()
