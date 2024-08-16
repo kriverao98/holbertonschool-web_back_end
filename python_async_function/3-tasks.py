47b8898998ebe34656b6f4242b66a9501f18b60f
kriverao98 <riveraocasiokoral29@gmail.com>
2024-08-15 20:04:24 -0400

    Refactor Python Async project: Remove unused code and update to use concurrent coroutines

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -2,11 +2,10 @@
 import asyncio
 from basic_async_syntax import wait_random
 
+
 def task_wait_random(max_delay: int) -> asyncio.Task:
-
-
     """
-    Create an asyncio task that waits for a random 
+    Create an asyncio task that waits for a random
     amount of time.
     :param max_delay: The maximum delay in seconds.
     :return: An asyncio Task object.
