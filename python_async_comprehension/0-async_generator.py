d60d3b91e3751749fc057df7e37a5b7bc9a91ebc
kriverao98 <riveraocasiokoral29@gmail.com>
2024-08-19 12:20:37 -0400

    refactor: Add type annotation to async_generator function in Python Async project

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,8 +2,10 @@
 """Coroutine called async_generator that takes no arguments."""
 import asyncio
 import random
+from typing import AsyncGenerator
 
-async def async_generator():
+
+async def async_generator() -> AsyncGenerator[float, None]:
     """
     Asynchronous generator that yields random delays between 0 and 10.
     """
